f5eecb66235dbdc66059d7fa49c31dce3badce6f
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2022-01-10 13:19:35 -0700

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Ryan Anderson on 1/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
