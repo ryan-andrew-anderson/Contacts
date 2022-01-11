896dcc10a19e10040284c421954bdf212ede6d15
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2022-01-10 22:01:52 -0700

    Working on Tableviews and SeachBar
    -Editing style Changes
    -FileManger saving and loading
    -adding UISearchBar Delegate

--- Contacts/Controller/ViewController.swift
+++ Contacts/Controller/ViewController.swift
@@ -9,16 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    var contact : Contact?
     var contacts = [
         Contact(name: "Ryan A. Anderson", number: "435-215-9307", email: "ryan.anderson@gmail.com"),
         Contact(name: "Kendra M. Anderson", number: "801-669-2931", email: "a@b.com")
     ]
     
+    var delegate: UISearchBarDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        searchBar.delegate = self
     }
     
     
@@ -35,6 +40,23 @@ extension ViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.reuseableCell, for: indexPath) as! TableViewCell
         cell.contactName.text = contacts[indexPath.row].name
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            contacts.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            print("Insert editing style")
+        }
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
 
--- Contacts/Model/Contact.swift
+++ Contacts/Model/Contact.swift
@@ -7,9 +7,27 @@
 
 import Foundation
 
-struct Contact {
+struct Contact: Codable {
     let name : String
     let number : String
     let email : String
     
+    static let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    
+    static let archiveURL = documentsDirectory.appendingPathComponent("Contacts").appendingPathExtension("plist")
+    
+    static func saveToFile(contacts: [Contact]) {
+        let propertyListEncoder = PropertyListEncoder()
+        let encodedContants = try? propertyListEncoder.encode(contacts)
+        try? encodedContants?.write(to: archiveURL, options: .noFileProtection)
+    }
+    
+    static func loadFromFile() -> [Contact] {
+        let propertyListDecoder = PropertyListDecoder()
+        if let retivedContactsData = try? Data(contentsOf: archiveURL),
+           let decodedContacts = try? propertyListDecoder.decode(Array <Contact>.self, from: retivedContactsData) {
+            return decodedContacts
+        }
+        return[]
+    }
 }
