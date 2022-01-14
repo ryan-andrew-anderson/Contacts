cdd1d8884cd4b8322b30ddd872b4c446d9cc9c40
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2022-01-14 11:43:04 -0700

    Confg. TableViewController and TableViewCell

---
+++ Contacts/Controller/ContantTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContantTableViewCell.swift
+//  Contacts
+//
+//  Created by Ryan Anderson on 1/14/22.
+//
+
+import UIKit
+
+class ContantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
